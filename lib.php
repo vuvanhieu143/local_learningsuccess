a53678e0971c29d52983ddb32712b40db8376cb0
hieuvu <hieuvu@nashtechglobal.com>
2026-09-09 23:39:38 +0700

    feat(core): plugin metadata, configuration settings, and course navigation integration
    - Define plugin version requirements for Moodle 5.0+ (build 2025041400) and PHP 8.2+
    - Add administration settings page under Reports category with configurable risk thresholds
    - Implement local_learningsuccess_extend_navigation_course to inject Learning Success tab into course secondary navigation
    - Add monochrome SVG logo for Moodle UI integration

---
+++ lib.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Standard plugin library callbacks for local_learningsuccess.
+ *
+ * @package    local_learningsuccess
+ * @copyright  2026 Learning Success Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Extends the course navigation menu with a direct link to Learning Success.
+ *
+ * @param navigation_node $parentnode The course node.
+ * @param stdClass $course The course object.
+ * @param context_course $context The course context.
+ */
+function local_learningsuccess_extend_navigation_course(
+    navigation_node $parentnode,
+    stdClass $course,
+    context_course $context
+): void {
+    if (!get_config('local_learningsuccess', 'enabled')) {
+        return;
+    }
+
+    if (has_capability('local/learningsuccess:viewcourse', $context)) {
+        $url = new moodle_url('/local/learningsuccess/dashboard.php', ['courseid' => $course->id]);
+        $node = navigation_node::create(
+            get_string('pluginname', 'local_learningsuccess'),
+            $url,
+            navigation_node::NODETYPE_LEAF,
+            null,
+            'local_learningsuccess',
+            new pix_icon('i/report', get_string('pluginname', 'local_learningsuccess'))
+        );
+        $node->showinflatnavigation = true;
+        $parentnode->add_node($node);
+    }
+}
---
+++ settings.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Admin settings for local_learningsuccess.
+ *
+ * @package    local_learningsuccess
+ * @copyright  2026 Learning Success Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage(
+        'local_learningsuccess',
+        get_string('pluginname', 'local_learningsuccess')
+    );
+
+    $ADMIN->add('localplugins', $settings);
+
+    if ($ADMIN->fulltree) {
+        // Enable plugin.
+        $settings->add(new admin_setting_configcheckbox(
+            'local_learningsuccess/enabled',
+            get_string('setting_enabled', 'local_learningsuccess'),
+            get_string('setting_enabled_desc', 'local_learningsuccess'),
+            1
+        ));
+
+        // Enable Class Pulse.
+        $settings->add(new admin_setting_configcheckbox(
+            'local_learningsuccess/enable_classpulse',
+            get_string('setting_enable_classpulse', 'local_learningsuccess'),
+            get_string('setting_enable_classpulse_desc', 'local_learningsuccess'),
+            1
+        ));
+
+        // Enable Intervention Tracking.
+        $settings->add(new admin_setting_configcheckbox(
+            'local_learningsuccess/enable_interventions',
+            get_string('setting_enable_interventions', 'local_learningsuccess'),
+            get_string('setting_enable_interventions_desc', 'local_learningsuccess'),
+            1
+        ));
+
+        // Inactivity threshold (days).
+        $settings->add(new admin_setting_configtext(
+            'local_learningsuccess/inactivity_threshold',
+            get_string('setting_inactivity_threshold', 'local_learningsuccess'),
+            get_string('setting_inactivity_threshold_desc', 'local_learningsuccess'),
+            7,
+            PARAM_INT
+        ));
+
+        // Grade decline threshold percentage.
+        $settings->add(new admin_setting_configtext(
+            'local_learningsuccess/grade_decline_threshold',
+            get_string('setting_grade_decline_threshold', 'local_learningsuccess'),
+            get_string('setting_grade_decline_threshold_desc', 'local_learningsuccess'),
+            20,
+            PARAM_INT
+        ));
+    }
+}
+
